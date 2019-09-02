bb67efaf8d414e99c54ab584badc7466ceac3390
LiuMo <mickyliumo@gmail.com>
2019-09-02 13:30:01 +0800

    Change function name

--- R/Functions.R
+++ R/Functions.R
@@ -2,7 +2,7 @@
 #'
 #'@description This function generates QBiC scores for all uPBM experiments for given twelvemers
 #'
-#'@param twelvemers.list A list of twelvemers, with a 11mer centered at the mutation base and 1 mutated base appended
+#'@param twelvemers A list of twelvemers, with a 11mer centered at the mutation base and 1 mutated base appended
 #'
 #'@param uPBM_QBiC_scores All QBiC scores for a universal PBM
 #'
@@ -10,10 +10,10 @@
 #'
 #'@export
 #'
-GenerateQBiCScoresFromTwelvemers <- function(uPBM_QBiC_scores,twelvemers.list) {
-  twelvemers.list <- data.frame(twelvemers.list)
-  seq_NA <- data.frame(setdiff(twelvemers.list[, 1], all.possible.twelvemers$seq))  ##select 12mers without mutation
-  seq_mut <- data.frame(setdiff(twelvemers.list[, 1], seq_NA[,1]))
+GenerateQBiCScoresFromTwelvemers <- function(uPBM_QBiC_scores, twelvemers) {
+  twelvemers <- data.frame(twelvemers)
+  seq_NA <- data.frame(setdiff(twelvemers[, 1], all.possible.twelvemers$seq))  ##select 12mers without mutation
+  seq_mut <- data.frame(setdiff(twelvemers[, 1], seq_NA[, 1]))
   colnames(seq_NA) <- "seq"
   colnames(seq_mut) <- "seq"
   seq_mut[, 1] <- as.character(seq_mut[, 1])
@@ -30,7 +30,7 @@ GenerateQBiCScoresFromTwelvemers <- function(uPBM_QBiC_scores,twelvemers.list) {
     seq_scores <- rbind(seq_scores, seq_NA)
   }
 
-  colnames(seq_scores) <- c("seq","QBiC.scores")
+  colnames(seq_scores) <- c("seq", "QBiC.scores")
 
   return(seq_scores)
 }
@@ -40,41 +40,44 @@ GenerateQBiCScoresFromTwelvemers <- function(uPBM_QBiC_scores,twelvemers.list) {
 #'
 #'@description This function generates a mutation spectrum of 96 mutation classes (mutations on pyrimidine centered trinucleotide) from a list of twelvemers
 #'
-#'@param twelvemers.list A list of twelvemers, with a 11mer centered at the mutation base and 1 mutated base appended
+#'@param twelvemers A list of twelvemers, with a 11mer centered at the mutation base and 1 mutated base appended
 #'
 #'@return A mutation spectrum with 96 mutation types
 #'
 #'@export
 #'
-Generate96ChannelSpectrumFromTwelvemers <- function(twelvemers.list) {
-  twelvemers.list <- data.frame(twelvemers.list)
-  twelvemers.list$ref <- substring(twelvemers.list[, 1], 6, 6)
-  for (i in 1:nrow(twelvemers.list)) {
-    if (twelvemers.list$ref[i] == "G" || twelvemers.list$ref[i] == "A") {
-      twelvemers.list$mutclass[i] <- paste(spgs::reverseComplement(substring(twelvemers.list[i, 1], 5, 7),case = "upper"),
-                                           spgs::reverseComplement(substring(twelvemers.list[i, 1], 12, 12),case = "upper"), sep = "")
+Generate96ChannelSpectrumFromTwelvemers <- function(twelvemers) {
+  twelvemers <- data.frame(twelvemers)
+  twelvemers$ref <- substring(twelvemers[, 1], 6, 6)
+  for (i in 1:nrow(twelvemers)) {
+    if (twelvemers$ref[i] == "G" || twelvemers$ref[i] == "A") {
+      twelvemers$mutclass[i] <- paste(spgs::reverseComplement(substring(twelvemers[i,
+                                                                                             1], 5, 7), case = "upper"), spgs::reverseComplement(substring(twelvemers[i,
+                                                                                                                                                                           1], 12, 12), case = "upper"), sep = "")
     } else {
-      twelvemers.list$mutclass[i] <- paste(substring(twelvemers.list[i, 1], 5, 7),
-                                                substring(twelvemers.list[i, 1], 12, 12), sep = "")
+      twelvemers$mutclass[i] <- paste(substring(twelvemers[i, 1],
+                                                     5, 7), substring(twelvemers[i, 1], 12, 12), sep = "")
     }
   }
 
-  mutation.spectrum <- data.frame(table(twelvemers.list$mutclass))
-  mutation.spectrum[, 2] <- as.numeric(mutation.spectrum[, 2])/sum(as.numeric(mutation.spectrum[,2]))
+  mutation.spectrum <- data.frame(table(twelvemers$mutclass))
+  mutation.spectrum[, 2] <- as.numeric(mutation.spectrum[, 2])/sum(as.numeric(mutation.spectrum[,
+                                                                                                2]))
 
   all.mutation.class <- data.frame(mutation.type.list)
 
-  all.mutation.class[,2] <- 0
+  all.mutation.class[, 2] <- 0
 
-  all.mutation.class[,2] <- mutation.spectrum[match(all.mutation.class[,1],mutation.spectrum[,1]),2]
+  all.mutation.class[, 2] <- mutation.spectrum[match(all.mutation.class[, 1], mutation.spectrum[,
+                                                                                                1]), 2]
 
-  if(sum(is.na(all.mutation.class[,2]))>0){
+  if (sum(is.na(all.mutation.class[, 2])) > 0) {
 
-    all.mutation.class[which(is.na(all.mutation.class[,2])),2] <- 0
+    all.mutation.class[which(is.na(all.mutation.class[, 2])), 2] <- 0
 
   }
 
-  colnames(all.mutation.class) <- c("mutclass","proportion")
+  colnames(all.mutation.class) <- c("mutclass", "proportion")
 
   all.mutation.class$mutclass <- as.character(all.mutation.class$mutclass)
 
@@ -107,15 +110,16 @@ SignatureQBiC <- function(uPBM_QBiC_scores, spectrum) {
   counts_signature <- 0
 
   for (mutation.type in mutation.type.list) {
-    histogram_plot_mutation <- graphics::hist(as.numeric(PBM.scores[which(PBM.scores$mutclass == mutation.type),1]),
-                                    breaks = seq(-number, number, 0.5), plot = F)
-    counts_temp <- as.numeric(histogram_plot_mutation$counts) * mutation.spectrum[mutation.type, 2]
+    histogram_plot_mutation <- graphics::hist(PBM.scores[which(PBM.scores$mutclass ==
+                                                                            mutation.type), 1], breaks = seq(-number, number, 0.5), plot = F)
+    counts_temp <- histogram_plot_mutation$counts * mutation.spectrum[mutation.type,
+                                                                                  2]
     counts_signature <- counts_signature + counts_temp
   }
   #######################################################################
-  histogram_plot_raw <- hist(as.numeric(PBM.scores[, 1]), breaks = seq(-number, number, 0.5), plot = F)
-  neg_index <- which(histogram_plot_raw$mids < 0)
-  pos_index <- which(histogram_plot_raw$mids > 0)
+  histogram_plot_raw <- hist(PBM.scores[, 1], breaks = seq(-number, number, 0.5), plot = F)
+  neg_index <- which(histogram_plot_raw$mids < 0) ##index of positive scores
+  pos_index <- which(histogram_plot_raw$mids > 0) ##index of negative scores
   neg.sum.raw <- sum(histogram_plot_raw$mids[neg_index] * histogram_plot_raw$counts[neg_index])/sum(histogram_plot_raw$counts[neg_index])
   pos.sum.raw <- sum(histogram_plot_raw$mids[pos_index] * histogram_plot_raw$counts[pos_index])/sum(histogram_plot_raw$counts[pos_index])
   normalized.counts_signature <- sum(histogram_plot_raw$counts) * counts_signature/sum(counts_signature)  #normalization
@@ -123,7 +127,7 @@ SignatureQBiC <- function(uPBM_QBiC_scores, spectrum) {
   neg.sum.weighted <- sum(histogram_plot_weighted$mids[neg_index] * normalized.counts_signature[neg_index])/sum(normalized.counts_signature[neg_index])
   pos.sum.weighted <- sum(histogram_plot_weighted$mids[pos_index] * normalized.counts_signature[pos_index])/sum(normalized.counts_signature[pos_index])
   list <- c(pos.sum.weighted, neg.sum.weighted)/c(pos.sum.raw, neg.sum.raw)
-  return(round(list,digits=3))
+  return(round(list, digits = 3))
 }
 
 
@@ -139,7 +143,8 @@ SignatureQBiC <- function(uPBM_QBiC_scores, spectrum) {
 #'
 #'@export
 #'
-GenerateSignatureWeightedQBiCScoresDistribution <- function(uPBM_QBiC_scores, spectrum, output_name) {
+GenerateSignatureWeightedQBiCScoresDistribution <- function(uPBM_QBiC_scores, spectrum,
+                                                            output_name) {
 
   uPBM_QBiC_scores <- data.frame(uPBM_QBiC_scores)
   uPBM_QBiC_scores <- uPBM_QBiC_scores$z_score[which(!is.na(uPBM_QBiC_scores$z_score))]
@@ -147,7 +152,7 @@ GenerateSignatureWeightedQBiCScoresDistribution <- function(uPBM_QBiC_scores, sp
 
   row.names(PBM.scores) <- all.possible.twelvemers$seq
   PBM.scores$mutclass <- all.possible.twelvemers$mutclass
-  number <- as.integer(max(PBM.scores[,1])) + 2
+  number <- as.integer(max(PBM.scores[, 1])) + 2
   densities.weighted <- 0
 
   mutation.spectrum <- check.spectrum(spectrum)
@@ -155,10 +160,9 @@ GenerateSignatureWeightedQBiCScoresDistribution <- function(uPBM_QBiC_scores, sp
   pdf(output_name, width = 16, height = 32)
   par(mfrow = c(8, 4))
   for (mutation.type in mutation.type.list) {
-    histogram_plot <- hist(as.numeric(PBM.scores[which(PBM.scores$mutclass ==
-                                                                    mutation.type),1]), breaks = seq(-number, number, 0.5), plot = F)
-    density_temp <- as.numeric(histogram_plot$density) * mutation.spectrum[mutation.type,
-                                                                              2]
+    histogram_plot <- hist(PBM.scores[which(PBM.scores$mutclass ==
+                                                         mutation.type), 1], breaks = seq(-number, number, 0.5), plot = F)
+    density_temp <- histogram_plot$density * mutation.spectrum[mutation.type, 2]
     densities.weighted <- densities.weighted + density_temp
     plot(histogram_plot, main = mutation.type, col = "black", freq = F, ylim = c(0,
                                                                                  ylimits))
@@ -166,11 +170,10 @@ GenerateSignatureWeightedQBiCScoresDistribution <- function(uPBM_QBiC_scores, sp
     plot(histogram_plot, main = mutation.type, col = "grey", freq = F, ylim = c(0,
                                                                                 ylimits))
   }
-  histogram_plot <- hist(as.numeric(PBM.scores[, 1]), breaks = seq(-number, number,
+  histogram_plot <- hist(PBM.scores[, 1], breaks = seq(-number, number,
                                                                    0.5), plot = F)
-  ylimits_raw <- max(histogram_plot$density)
-  ylimits_weighted <- max(densities.weighted)
-  ylimits <- max(c(ylimits_raw, ylimits_weighted)) + 0.05
+
+  ylimits <- max(c(histogram_plot$density, densities.weighted)) + 0.05
   plot(histogram_plot, col = "black", freq = F, ylim = c(0, ylimits))
   histogram_plot$density <- densities.weighted
   plot(histogram_plot, col = "grey", freq = F, ylim = c(0, ylimits))
@@ -198,8 +201,8 @@ Pathway_Selection <- function(TF.list, qvalue, dbs.selected) {
   enriched.matrix$TFs <- apply(enriched.matrix.gain, 1, function(x) {
     x["TFs"] <- unlist(strsplit(x["Overlap"], "/"))[1]
   })
-  enriched.matrix <- enriched.matrix[enriched.matrix$Adjusted.P.value <
-                                                 qvalue, ]
+  enriched.matrix <- enriched.matrix[enriched.matrix$Adjusted.P.value < qvalue,
+                                     ]
   enriched.matrix$New.Term <- apply(enriched.matrix, 1, function(x) {
     x["New.Term"] <- unlist(strsplit(x["Term"], "[_]", perl = T))[1]
   })
@@ -221,7 +224,7 @@ Pathway_Selection <- function(TF.list, qvalue, dbs.selected) {
 MutationTypeContribution <- function(uPBM_QBiC_scores, spectrum) {
   summary.matrix <- data.frame(mutation.type.list)
   summary.matrix[, 2:5] <- 0
-  summary.matrix[,1] <- as.character(summary.matrix[,1])
+  summary.matrix[, 1] <- as.character(summary.matrix[, 1])
   uPBM_QBiC_scores <- data.frame(uPBM_QBiC_scores)
   uPBM_QBiC_scores <- uPBM_QBiC_scores$z_score[which(!is.na(uPBM_QBiC_scores$z_score))]
   PBM.scores <- data.frame(uPBM_QBiC_scores)
@@ -234,16 +237,19 @@ MutationTypeContribution <- function(uPBM_QBiC_scores, spectrum) {
   k <- 0
   for (mutation.type in mutation.type.list) {
     k <- k + 1
-    histogram_plot <- hist(as.numeric(PBM.scores[which(PBM.scores$mutclass ==
-                                                                    mutation.type),1]), breaks = seq(-number, number, 0.5), plot = F)
-    counts_signature.temp <- as.numeric(histogram_plot$counts) * mutation.spectrum[mutation.type, 2]
+    histogram_plot <- hist(PBM.scores[which(PBM.scores$mutclass ==
+                                                         mutation.type), 1], breaks = seq(-number, number, 0.5), plot = F)
+    counts_signature.temp <- histogram_plot$counts * mutation.spectrum[mutation.type,
+                                                                                   2]
     neg_index <- which(histogram_plot$mids < 0)
     pos_index <- which(histogram_plot$mids > 0)
     summary.matrix[k, 2] <- sum(histogram_plot$mids[neg_index] * counts_signature.temp[neg_index])
     summary.matrix[k, 3] <- sum(histogram_plot$mids[pos_index] * counts_signature.temp[pos_index])
   }
-  summary.matrix[, 4] <- as.numeric(summary.matrix[, 3])/sum(as.numeric(summary.matrix[, 3]))
-  summary.matrix[, 5] <- as.numeric(summary.matrix[, 2])/sum(as.numeric(summary.matrix[, 2]))
+  summary.matrix[, 4] <- as.numeric(summary.matrix[, 3])/sum(as.numeric(summary.matrix[,
+                                                                                       3]))
+  summary.matrix[, 5] <- as.numeric(summary.matrix[, 2])/sum(as.numeric(summary.matrix[,
+                                                                                       2]))
   summary.matrix <- summary.matrix[, c(1, 4, 5)]  ##this gives a contribution of all final scores, in another word: all sums
   colnames(summary.matrix) <- c("Mutation.Class", "Contribute.to.Gain", "Contribute.to.Loss")
   return(summary.matrix)
@@ -258,15 +264,16 @@ MutationTypeContribution <- function(uPBM_QBiC_scores, spectrum) {
 #'
 #'@keywords internal
 
-check.spectrum <- function(spectrum){
+check.spectrum <- function(spectrum) {
 
   spectrum.96.channel <- data.frame(mutation.type.list)
 
   spectrum.96.channel$possibility <- 0
 
-  spectrum.96.channel$possibility <- spectrum[match(row.names(spectrum),spectrum.96.channel[,1]),1]
+  spectrum.96.channel$possibility <- spectrum[match(row.names(spectrum), spectrum.96.channel[,
+                                                                                             1]), 1]
 
-  if(sum(is.na(spectrum.96.channel$possibility))>0){
+  if (sum(is.na(spectrum.96.channel$possibility)) > 0) {
     spectrum.96.channel$possibility[which(is.na(spectrum.96.channel$possibility))] <- 0
   }
 
